ede8edbe399787235eeb2c96011ff535580e5b13
Aquilino Cagubcub <56160486+linoqui14@users.noreply.github.com>
2023-12-11 02:50:31 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    future: { webpack5: true }
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module
+        if (!isServer) {
+          config.node = {
+            fs: 'empty'
+          }
+        }
+    
+        return config
+      }
 }
 
 module.exports = nextConfig
