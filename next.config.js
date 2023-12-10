395e76ba698dc937622ff3230908eb85c11ffbc7
Aquilino Cagubcub <56160486+linoqui14@users.noreply.github.com>
2023-12-11 03:00:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         // Fixes npm packages that depend on `fs` module
         if (!isServer) {
           config.node = {
-            fs: 'empty'
+           
           }
         }
     
